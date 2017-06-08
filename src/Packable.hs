8c0aa1f8a47d2942ad054de08c3278fc7fef75b3
Zebulun Arendsee <arendsee@iastate.edu>
2017-06-08 08:58:53 -0500

    Update documentation

--- src/Packable.hs
+++ src/Packable.hs
@@ -1,14 +1,40 @@
--- | Allow multiple parameters in one class
+-- Allow multiple parameters in one class
 {-# LANGUAGE MultiParamTypeClasses #-}
--- | Allow parameterized types in function context
+-- Allow parameterized types in function context
 {-# LANGUAGE FlexibleContexts #-}
--- | Allow parameterized types in instances
+-- Allow parameterized types in instances
 {-# LANGUAGE FlexibleInstances #-}
--- | Allow type synonyms in instance contexts
---   Haskell98 only allows type constructors made with newtype and data
+-- Allow type synonyms in instance contexts
+-- Haskell98 only allows type constructors made with newtype and data
 {-# LANGUAGE TypeSynonymInstances #-}
--- | Needed for string literals to be interpreted as Text or ByteString
+-- Needed for string literals to be interpreted as Text or ByteString
 {-# LANGUAGE OverloadedStrings #-}
+
+{-|
+
+The package exports the typeclass 'Packable', which contains two main
+functions: 'gpack' and 'gunpack'. these are symmetric functions, where @'gpack' .
+'gunpack'@ and @'gunpack' . 'gpack'@ are both defined and both identical to @'id'@.
+members of the 'Packable' typeclass are assumed to be things can be converted
+to or from a reduced form. the packable typeclass takes two parameters, the
+first is the unpacked type, the second is the packed type.  here are a few
+examples of type pairs.
+
+  * unencrypted data, encrypted data
+  * uncompressed data, compressed data
+  * a nested list of files, an archive
+  * a haskell object, a binary serialization
+  * a haskell object, a json text file
+  * string, bytestring
+  * string, text
+  * bytestring, text
+
+often times there can be many layers of packing, for example a haskell
+object that has ben serialized, compressed, and encrypted. this typeclass
+would ideally allow this to be opened with an @'gunpack' . 'gunpack' . 'gunpack'@
+function.
+
+-}
 
 module Packable
 (
@@ -17,33 +43,45 @@ module Packable
 ) where
 
 import qualified Data.ByteString.Char8 as DBC
-import qualified Data.String
 import qualified Data.Text as DT
 
--- | This is a class of symetric unary functions.  Requires:
---   (1) MultiParamTypeClasses to have two types 
+-- Requires: MultiParamTypeClasses to have two types 
+
+-- | A class for symmetric types 'a' and 'b', where 'a' is the unpacked form
+-- and 'b' is the packed form.
 class Packable a b where
-  gpack :: a -> b
+  gpack   :: a -> b
   gunpack :: b -> a
 
--- | The use of String in the context requires
---   (1) FlexibleContexts to allow a non-type variable ([Char])
---   (2) TypeSynonymInstances to allow String (rather than [Char])
+-- The use of String in the context requires
+-- (1) FlexibleContexts to allow a non-type variable ([Char])
+-- (2) TypeSynonymInstances to allow String (rather than [Char])
 instance Packable String DBC.ByteString where
-  gpack = DBC.pack
+  gpack   = DBC.pack
   gunpack = DBC.unpack
 
 instance Packable String DT.Text where
-  gpack = DT.pack
+  gpack   = DT.pack
   gunpack = DT.unpack
 
--- | Requires FlexibleContexts
---
 -- Without FlexibleContexts, emits this error message:
 --
 -- > Non type-variable argument in the constraint: Packable String a
 -- > ||   (Use FlexibleContexts to permit this)
 -- > || • In the type signature:
 -- > ||     unpackString :: (Packable String a) => a -> String
+--
+-- | 'unpackString' takes a Text or ByteString and converts it to String
+--
+-- @
+-- ghci> let s = "this is Text" :: Text
+-- ghci> let b = "this is a ByteString" :: ByteString
+-- ghci> unpackString s
+--   "this is Text"
+--   it :: String
+-- ghci> unpackString b
+--   "this is a ByteString"
+--   it :: String
+-- @
 unpackString :: (Packable String a) => a -> String
 unpackString = gunpack
